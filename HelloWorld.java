63358349978035d7ad6bcabad4ceedea936acede
Tan Tien Ping <tienping@usm.my>
2025-11-19 08:54:30 +0800

    Version 1: My HelloWorld program is created.

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+
+/**
+Class name: HelloWorld
+**/
+public class HelloWorld{
+
+  //A special main() static function. 
+	public static void main(String[] arg){
+        int x = 6;
+        assert x > 0: "X is more than 0";
+
+	}
+}
+
+ 
